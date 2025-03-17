582ccd5080242e46d06492b359b08afb97331a06
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-17 09:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
+	let table = document.querySelector("#sampleTable")
+	let row = document.createElement("tr")
+	row.innerHTML = `
+		<td>New Cell1</td> 
+		<td>New Cell2</td>`
+	table.insertBefore(row, table.children[0])
   
   
 }
